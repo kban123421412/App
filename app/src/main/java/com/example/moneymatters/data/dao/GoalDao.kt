6f4dcf47a688b6a2fea1d4a31599f75faa9340d2
krishan <krishanbanga9@gmail.com>
2026-05-11 17:58:15 +0100

    New goal dao

---
+++ app/src/main/java/com/example/moneymatters/data/dao/GoalDao.kt
@@ -0,0 +1,19 @@
+package com.example.moneymatters.data.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.moneymatters.data.model.GoalModel
+
+
+@Dao
+interface GoalDao{
+    @Insert
+    fun insertGoal(goal: GoalModel)
+
+    @Update
+    fun updateGoal(goal: GoalModel)
+
+    @Query("SELECT * FROM GoalModel ORDER BY ID DESC")
+    fun getAllGoals(): LiveData<List<GoalModel>>
+
+}
