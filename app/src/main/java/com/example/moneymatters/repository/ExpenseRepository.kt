a7cb16eaa7d287b0a740554d746773367106fcec
krishan <krishanbanga9@gmail.com>
2026-05-11 18:01:31 +0100

    Added goal logic

--- app/src/main/java/com/example/moneymatters/repository/ExpenseRepository.kt
+++ app/src/main/java/com/example/moneymatters/repository/ExpenseRepository.kt
@@ -4,9 +4,12 @@ import androidx.lifecycle.LiveData
 import com.example.moneymatters.data.dao.CategoryTotal
 import com.example.moneymatters.data.dao.ExpenseDao
 import com.example.moneymatters.data.model.ExpenseModel
+import com.example.moneymatters.data.dao.GoalDao
+import com.example.moneymatters.data.model.GoalModel
 
-class ExpenseRepository(private val dao:ExpenseDao) {
+class ExpenseRepository(private val dao:ExpenseDao, private val goalDao: GoalDao) {
 
+    // --Expense Logic--
     val allExpenses: LiveData<List<ExpenseModel>> = dao.getAllExpenses()
     val totalAmount: LiveData<Double> = dao.getTotalAmount()
 
@@ -23,4 +26,18 @@ class ExpenseRepository(private val dao:ExpenseDao) {
 
 
     }
+
+    // --GOAL LOGIC--
+
+    val allGoals : LiveData<List<GoalModel>> = goalDao.getAllGoals()
+
+    suspend fun insertGoal(goal: GoalModel) {
+        goalDao.insertGoal(goal)
+    }
+
+    suspend fun updateGoal(goal: GoalModel) {
+        goalDao.updateGoal(goal)
+    }
+
+
 }
