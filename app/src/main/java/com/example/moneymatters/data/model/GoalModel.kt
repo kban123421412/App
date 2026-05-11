5bbb6638ebf4cd7f86f91d643391a19bdb845c86
krishan <krishanbanga9@gmail.com>
2026-05-11 17:57:57 +0100

    New goal model

---
+++ app/src/main/java/com/example/moneymatters/data/model/GoalModel.kt
@@ -0,0 +1,13 @@
+package com.example.moneymatters.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (goal_table)
+data class GoalModel {
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val targetAmount: Double,
+    var currentAmount: Double = 0.0,
+
+}
